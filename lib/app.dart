368a1d0e8246d694b85be06d136e02cb51ae9074
Abo Zeid <hamoashraf1960@gmail.com>
2023-05-20 15:12:01 +0300

    Packages

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TouchLine extends StatelessWidget {
+  const TouchLine({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        builder: (context, child){
+
+        }
+
+    );
+  }
+}
